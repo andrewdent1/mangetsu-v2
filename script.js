548b2745e118a8952acb6ba59dcfc6b5ce9608aa
Andrew Dent <andrew.dent@bbrown.com>
2024-10-31 14:54:03 -0400

    complete nav

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    const scrollPosition = window.scrollY;
 
+    if (scrollPosition > 150) {
+        header.classList.add('header-colored');
+    } else {
+        header.classList.remove('header-colored');
+    }
+});
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
+document.querySelector('.close-btn').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default action
+    // Code to close the sidebar
+});
